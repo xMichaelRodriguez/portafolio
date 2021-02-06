c5140631236766c87b436b9a49b06fcb033dcbe0
xMichaelRodriguez <scottlovos503@gmail.com>
2021-02-06 17:01:30 -0600

    envio de correo

---
+++ assets/js/sendMessage.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  event.preventDefault();
+
+  const btn = document.getElementById("btnSend");
+  btn.addEventListener("click", () => {
+    let user = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let body = document.getElementById("message").value;
+
+    const data = {
+      user,
+      email,
+      body,
+    };
+
+    const correoReg = /^[a-zA-Z0-9{3}+]+\S[a-zA-Z0-9]+\S@\S[a-zA-Z0-9_.-]+\.[a-zA-Z]+[^\s]$/;
+    if (correoReg.test(email)) {
+      if (user & body) {
+        fetch("http://localhost:3000/contact/", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        })
+          .then((resp) => resp.json())
+          .then((resp) => {
+            Swal.fire({
+              icon: resp.status,
+              title: "Exito!",
+              text: resp.message,
+            }).then((result) => {
+              if (result.isConfirmed) {
+                document.getElementById("name").value = "";
+                document.getElementById("email").value = "";
+                document.getElementById("message").value = "";
+              }
+            });
+          })
+          .catch(console.warn);
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Upss...",
+          text: "Rellana los campos faltantes",
+        });
+      }
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Upss...",
+        text: "Correo invalido",
+      });
+    }
+  });
+});
